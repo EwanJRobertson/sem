1251c8f27dfe11a0deb1611d349b0d0d42cdd628
Ewan <40451077@live.napier.ac.uk>
2021-02-04 12:21:52 +0000

    mongodb server

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,7 +16,7 @@ public class App
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Ewan-Robertson")
+        Document doc = new Document("name", "Ewan Robertson")
                 .append("class", "Software Engineering Methods")
                 .append("year", "2021")
                 .append("result", new Document("CW", 95).append("EX", 85));
